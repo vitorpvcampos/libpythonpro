58015dea03824f8edd9a15cb30e799f73bb4e0a8
Vitor Campos <vitor.campos@engenharia.ufjf.br>
2020-04-02 16:11:00 -0300

    Adicionada flake8 como pendência

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -8,9 +8,10 @@ def buscar_avatar(usuario):
     :param usuario: str com o nome de usuário no github
     :return: str com o link do avatar
     """
-    url=f'https://api.github.com/users/{usuario}'
+    url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
 
 if __name__ == '__main__':
     print(buscar_avatar('vitorpvcampos'))
