83322a7c87ae8420335ae900404b3a22841721ed
Vitor Campos <vitor.campos@engenharia.ufjf.br>
2020-04-04 13:35:30 -0300

    Implementado testes de spam

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,38 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['vitor.campos@engenharia.ufjf.br', 'aumgn@sociedadenovoaeon.org']
+)
+def test_remetente(remetente):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        remetente,
+        'aumgn@sociedadenovoaeon.org',
+        'Assunto Teste',
+        'Corpo do email de teste.'
+    )
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'aumgn']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        resultado = enviador.enviar(
+            remetente,
+            'aumgn@sociedadenovoaeon.org',
+            'Assunto Teste',
+            'Corpo do email de teste.'
+        )
